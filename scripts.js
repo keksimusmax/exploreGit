b06edaeb4fbe775d2a2677307b6244c5d2fa57e9
Mateusz Kaczmarek <mateusz.kaczmarek@finder.com>
2016-12-26 13:36:06 +0100

    master closures excercise

--- scripts.js
+++ scripts.js
@@ -1,3 +1,16 @@
 var textToPut = prompt("Insert heading");
 var h1 = document.querySelector('h1');
 h1.innerHTML += textToPut;
+
+//closures
+var add = (function() {
+	var counter = 0;
+	return function() {
+		console.log(counter);
+		counter += 1;
+	}
+})();
+
+add();
+add();
+add();
